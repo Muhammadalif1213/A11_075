9b9266d1f927050dab7126387c83b15a62cb8609
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-01-22 15:15:23 +0700

    Membuat destinasi navigasi dan menambahkan alamat navigasi input data buku.

---
+++ app/src/main/java/com/example/uas_pam/ui/Navigation/DestinasiNavigasi.kt
@@ -0,0 +1,12 @@
+package com.example.uas_pam.ui.Navigation
+
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: String
+}
+
+object DestinasiEntryBuku: DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes= "Entry Buku"
+}
+
