4aa8e3a04e358ec13d2dd878c5ba7152830ef114
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-01-22 14:46:21 +0700

    Menambahkan fungsi InsertBukuUiEvent, InsertBukuUiState, dan Buku untuk mengelola perubahan data dalam state dan model.

--- app/src/main/java/com/example/uas_pam/ui/viewModel/Buku/insertBukuViewModel.kt
+++ app/src/main/java/com/example/uas_pam/ui/viewModel/Buku/insertBukuViewModel.kt
@@ -1,6 +1,30 @@
 package com.example.uas_pam.ui.viewModel.Buku
 
 
+import com.example.uas_pam.model.Buku
+
+
+
+fun InsertBukuUiEvent.toBuku(): Buku = Buku(
+    idBuku = idBuku,
+    judul = judul,
+    penulis = penulis,
+    kategori = kategori,
+    status = status
+)
+
+fun Buku.toInsertUiStateBuku(): InsertBukuUiState = InsertBukuUiState(
+    insertBukuUiEvent = toInsertBukuUiEvent()
+)
+
+fun Buku.toInsertBukuUiEvent(): InsertBukuUiEvent = InsertBukuUiEvent(
+    idBuku = idBuku,
+    judul = judul,
+    penulis = penulis,
+    kategori = kategori,
+    status = status
+)
+
 data class InsertBukuUiState(
     val insertBukuUiEvent: InsertBukuUiEvent = InsertBukuUiEvent()
 )
