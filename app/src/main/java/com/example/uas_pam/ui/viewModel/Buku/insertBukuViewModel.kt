322fe745b809f0eb992822d3070d6c5484e77bb2
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-01-22 15:08:38 +0700

    Menambahkan class InsertBukuViewModel untuk mengatur inputan data.

--- app/src/main/java/com/example/uas_pam/ui/viewModel/Buku/insertBukuViewModel.kt
+++ app/src/main/java/com/example/uas_pam/ui/viewModel/Buku/insertBukuViewModel.kt
@@ -1,9 +1,32 @@
 package com.example.uas_pam.ui.viewModel.Buku
 
-
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.uas_pam.model.Buku
+import com.example.uas_pam.repository.BukuRepository
+import kotlinx.coroutines.launch
 
+class InsertBukuViewModel(private val bk: BukuRepository): ViewModel() {
+    var uiState by mutableStateOf(InsertBukuUiState())
+        private set
 
+    fun updateInsertBukuState(insertUiEvent: InsertBukuUiEvent){
+        uiState = InsertBukuUiState(insertUiEvent)
+    }
+
+    suspend fun insertBuku(){
+        viewModelScope.launch {
+            try {
+                bk.insertBuku(uiState.insertBukuUiEvent.toBuku())
+            }catch (e: Exception){
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 fun InsertBukuUiEvent.toBuku(): Buku = Buku(
     idBuku = idBuku,
