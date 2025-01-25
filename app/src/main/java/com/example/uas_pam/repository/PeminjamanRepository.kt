283dd0e2a6d7a30d7619475f998cffdb96d51be8
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-01-25 10:52:30 +0700

    menambahkan fungsi detail, delete, update di peminjaman servis dan repository.

--- app/src/main/java/com/example/uas_pam/repository/PeminjamanRepository.kt
+++ app/src/main/java/com/example/uas_pam/repository/PeminjamanRepository.kt
@@ -5,11 +5,45 @@ import com.example.uas_pam.service_api.PeminjamanService
 
 interface PeminjamanRepository{
     suspend fun getPeminjaman(): List<Peminjaman>
+    suspend fun insertPeminjaman(peminjaman: Peminjaman)
+    suspend fun getPeminjamanById(idPeminjaman: Int): Peminjaman
+    suspend fun editPeminjaman(idPeminjaman: Int, peminjaman: Peminjaman)
+    suspend fun deletePeminjaman(idPeminjaman: Int)
+
 }
 
 class NetworkPeminjamanRepository(
     private val peminjamanApiService: PeminjamanService
 ): PeminjamanRepository {
     override suspend fun getPeminjaman(): List<Peminjaman> = peminjamanApiService.getPeminjaman()
+
+    override suspend fun insertPeminjaman(peminjaman: Peminjaman) {
+        peminjamanApiService.insertPeminjaman(peminjaman)
+    }
+
+    override suspend fun getPeminjamanById(idPeminjaman: Int): Peminjaman {
+        return peminjamanApiService.getPeminjamanById(idPeminjaman)
+    }
+
+    override suspend fun editPeminjaman(idPeminjaman: Int, peminjaman: Peminjaman) {
+        peminjamanApiService.editPeminjaman(idPeminjaman, peminjaman)
+    }
+
+    override suspend fun deletePeminjaman(idPeminjaman: Int) {
+        try {
+            val response = peminjamanApiService.deletePeminjaman(idPeminjaman)
+            if (!response.isSuccessful) {
+                throw Exception(
+                    "Failed to delete peminjaman with ID: " +
+                            "${response.code()}"
+                )
+            }else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
 
 }
--- app/src/main/java/com/example/uas_pam/service_api/PeminjamanService.kt
+++ app/src/main/java/com/example/uas_pam/service_api/PeminjamanService.kt
@@ -1,8 +1,14 @@
 package com.example.uas_pam.service_api
 
 import com.example.uas_pam.model.Peminjaman
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface PeminjamanService {
     @Headers(
@@ -11,4 +17,16 @@ interface PeminjamanService {
     )
     @GET("bacapeminjaman.php")
     suspend fun getPeminjaman(): List<Peminjaman>
+
+    @POST("insertpeminjaman.php")
+    suspend fun insertPeminjaman(@Body peminjaman: Peminjaman)
+
+    @GET("bacapeminjaman.php")
+    suspend fun getPeminjamanById(@Query("id_peminjaman") idPeminjaman: Int): Peminjaman
+
+    @PUT("editpeminjaman.php/{id_peminjaman}")
+    suspend fun editPeminjaman(@Query("id_peminjaman") idPeminjaman: Int, @Body peminjaman: Peminjaman)
+
+    @DELETE("deletepeminjaman.php/{id_peminjaman}")
+    suspend fun deletePeminjaman(@Query("id_peminjaman") idPeminjaman: Int): Response<Void>
 }
