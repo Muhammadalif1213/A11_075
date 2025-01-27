5a6399d4245c3170912ffc57490ba6f186d8b58a
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-01-27 14:20:15 +0700

    Menambahkan fungsi insert, get by id, edit, delete pada repository pengembalian.

--- app/src/main/java/com/example/uas_pam/repository/PengembalianRepository.kt
+++ app/src/main/java/com/example/uas_pam/repository/PengembalianRepository.kt
@@ -2,10 +2,11 @@ package com.example.uas_pam.repository
 
 import com.example.uas_pam.model.Pengembalian
 import com.example.uas_pam.service_api.PengembalianService
+import java.io.IOException
 
 interface PengembalianRepository{
     suspend fun getPengembalian(): List<Pengembalian>
-    suspend fun insertPengembalian(peminjaman: Pengembalian)
+    suspend fun insertPengembalian(pengembalian: Pengembalian)
     suspend fun getPengembalianById(idPengembalian: Int): Pengembalian
     suspend fun editPengembalian(idPengembalian: Int, peminjaman: Pengembalian)
     suspend fun deletePengembalian(idPengembalian: Int)
@@ -18,20 +19,33 @@ class NetworkPengembalianRepository(
         return pengembalianApiService.getPengembalian()
     }
 
-    override suspend fun insertPengembalian(peminjaman: Pengembalian) {
-        TODO("Not yet implemented")
+    override suspend fun insertPengembalian(pengembalian: Pengembalian) {
+        pengembalianApiService.insertPengembalian(pengembalian)
     }
 
     override suspend fun getPengembalianById(idPengembalian: Int): Pengembalian {
-        TODO("Not yet implemented")
+        return  pengembalianApiService.getPengembalianById(idPengembalian)
     }
 
-    override suspend fun editPengembalian(idPengembalian: Int, peminjaman: Pengembalian) {
-        TODO("Not yet implemented")
+    override suspend fun editPengembalian(idPengembalian: Int, pengembalian: Pengembalian) {
+        pengembalianApiService.editPengembalian(idPengembalian,pengembalian)
     }
 
     override suspend fun deletePengembalian(idPengembalian: Int) {
-        TODO("Not yet implemented")
+        try {
+            val response = pengembalianApiService.deletePengembalian(idPengembalian)
+            if (!response.isSuccessful) {
+                throw IOException(
+                    "Failed to delete pengembalian with ID: " +
+                            "${response.code()}"
+                )
+                }else {
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
     }
 
 }
