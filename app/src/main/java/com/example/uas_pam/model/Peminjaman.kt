33d4d62e36affefb85810f356e7adf9c11e9ff28
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-01-28 02:16:41 +0700

    Menambahkan variabel nama, judul, tanggal pengembalian, dan status di data class peminjaman .

--- app/src/main/java/com/example/uas_pam/model/Peminjaman.kt
+++ app/src/main/java/com/example/uas_pam/model/Peminjaman.kt
@@ -8,15 +8,23 @@ data class Peminjaman (
     @SerialName("id_peminjaman")
     val idPeminjaman: Int,
 
+    @SerialName("nama_anggota")
+    val namaAnggota: String,
+
     @SerialName("id_buku")
     val idBuku: Int,
 
     @SerialName("id_anggota")
     val idAnggota: Int,
 
+    @SerialName("judul_buku")
+    val judulBuku: String,
+
     @SerialName("tanggal_peminjaman")
     val tanggalPeminjaman: String,
 
     @SerialName("tanggal_pengembalian")
-    val tanggalPengembalian: String
+    val tanggalPengembalian: String,
+
+    val status: String
 )
