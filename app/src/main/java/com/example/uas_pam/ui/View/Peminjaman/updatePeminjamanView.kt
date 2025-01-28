1d49f2015720aa3dd2c38700c621013b740af159
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-01-28 23:05:53 +0700

    Memperbaiki tampilan update peminjaman dengan card.

--- app/src/main/java/com/example/uas_pam/ui/View/Peminjaman/updatePeminjamanView.kt
+++ app/src/main/java/com/example/uas_pam/ui/View/Peminjaman/updatePeminjamanView.kt
@@ -1,12 +1,21 @@
 package com.example.uas_pam.ui.View.Peminjaman
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.uas_pam.ui.CustomWidget.CustomTopAppBar
 import com.example.uas_pam.ui.Navigation.DestinasiUpdatePeminjaman
@@ -39,19 +48,33 @@ fun updatePeminjamanView(
         },
         modifier = modifier
     ){ innerPadding ->
-        EntryBody(
-            insertUiState = updateViewModel.updateUiState,
-            onPeminjamanValueChange = updateViewModel::updatePeminjamanState,
-            onSaveClick = {
-                coroutineScope.launch {
-                    updateViewModel.updatePeminjaman()
-                    navigateBack()
-                }
-            },
-            modifier = Modifier.padding(innerPadding),
-            anggotaOptions = viewModel.anggotaOptions,
-            bukuOptions = viewModel.bukuOptions
-        )
+        Column (
+            modifier = Modifier.padding(innerPadding)
+                .background(Color(0xFF2196F3))
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+            Card(
+                modifier = Modifier.padding(16.dp),
+                elevation = CardDefaults.cardElevation(6.dp)
+            ){
+                EntryBody(
+                    insertUiState = updateViewModel.updateUiState,
+                    onPeminjamanValueChange = updateViewModel::updatePeminjamanState,
+                    onSaveClick = {
+                        coroutineScope.launch {
+                            updateViewModel.updatePeminjaman()
+                            navigateBack()
+                        }
+                    },
+                    modifier = Modifier,
+                    anggotaOptions = viewModel.anggotaOptions,
+                    bukuOptions = viewModel.bukuOptions
+                )
+            }
+        }
+
 
     }
 }
