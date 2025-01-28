1e5786c0becff2fe315ee3309149703aa78b5f42
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-01-28 15:57:04 +0700

    Membuat fungsi dropdown untuk inputan kategori dan status.

--- app/src/main/java/com/example/uas_pam/ui/View/Buku/insertBukuView.kt
+++ app/src/main/java/com/example/uas_pam/ui/View/Buku/insertBukuView.kt
@@ -8,15 +8,23 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Scaffold
 
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
@@ -35,6 +43,7 @@ import kotlinx.coroutines.launch
 fun EntryBukuScreen(
     navigateBack: () -> Unit,
     modifier: Modifier = Modifier,
+    onBackClick: () -> Unit,
     viewModel: InsertBukuViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
     val coroutineScope = rememberCoroutineScope()
@@ -57,7 +66,7 @@ fun EntryBukuScreen(
             onSaveClick = {
                 coroutineScope.launch {
                     viewModel.insertBuku()
-                    navigateBack()
+                    onBackClick()
                 }
             },
             modifier = Modifier
@@ -101,41 +110,51 @@ fun FormInput(
     modifier: Modifier = Modifier,
     onValueChange: (InsertBukuUiEvent) -> Unit = {},
     enabled: Boolean = true,
-){
-    Column (
+) {
+    val kategoriOptions = listOf("Novel", "Komik", "Fiksi", "Non-Fiksi")
+    val statusOptions = listOf("Dipinjam", "Tersedia")
+    var expanded by remember { mutableStateOf(false) }
+
+    Column(
         modifier = modifier,
         verticalArrangement = Arrangement.spacedBy(12.dp)
-    ){
+    ) {
         OutlinedTextField(
             value = insertUiEvent.judul,
-            onValueChange = {onValueChange(insertUiEvent.copy(judul = it))},
+            onValueChange = { onValueChange(insertUiEvent.copy(judul = it)) },
             label = { Text(text = "Judul") },
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
         )
+
         OutlinedTextField(
             value = insertUiEvent.penulis,
-            onValueChange = {onValueChange(insertUiEvent.copy(penulis = it))},
+            onValueChange = { onValueChange(insertUiEvent.copy(penulis = it)) },
             label = { Text(text = "Penulis") },
             modifier = Modifier.fillMaxWidth(),
             enabled = enabled,
             singleLine = true
         )
-        OutlinedTextField(
-            value = insertUiEvent.kategori,
-            onValueChange = {onValueChange(insertUiEvent.copy(kategori = it))},
-            label = { Text(text = "Kategori") },
-            modifier = Modifier.fillMaxWidth(),
-            enabled = enabled,
-            singleLine = true
+
+        DropDownTextField(
+            selectedValue = insertUiEvent.kategori,
+            options = kategoriOptions,
+            label = "Kategori",
+            onValueChangedEvent = { selectedKategori ->
+                onValueChange(insertUiEvent.copy(kategori = selectedKategori))
+            },
+            modifier = Modifier.fillMaxWidth()
         )
-        OutlinedTextField(
-            value = insertUiEvent.status,
-            onValueChange = {onValueChange(insertUiEvent.copy(status = it))},
-            label = { Text(text = "Status") },
-            modifier = Modifier.fillMaxWidth(),
-            enabled = enabled,
+
+        DropDownTextField(
+            selectedValue = insertUiEvent.status,
+            options = statusOptions,
+            label = "Status",
+            onValueChangedEvent = { selectedStatus ->
+                onValueChange(insertUiEvent.copy(status = selectedStatus))
+            },
+            modifier = Modifier.fillMaxWidth()
         )
 
         if (enabled){
@@ -148,5 +167,54 @@ fun FormInput(
             thickness = 8.dp,
             modifier = Modifier.padding(20.dp)
         )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DropDownTextField(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangedEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
+){
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded},
+        modifier = modifier
+    ) {
+        OutlinedTextField(
+            readOnly = true,
+            value = selectedValue,
+            onValueChange = {},
+            label = { Text(text = label ) },
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(
+                    expanded = expanded
+                )
+            },
+            colors = OutlinedTextFieldDefaults.colors(),
+            modifier = Modifier
+                .menuAnchor()
+                .fillMaxWidth()
+        )
+
+        ExposedDropdownMenu(expanded = expanded,
+            onDismissRequest = {expanded = false}) {
+            options.forEach{ option: String ->
+                DropdownMenuItem(
+                    text = { Text(text = option) },
+                    onClick = {
+                        expanded = false
+                        onValueChangedEvent(option)
+                    }
+                )
+            }
+        }
     }
 }
