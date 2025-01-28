2492fe9f83804f95ed37d5f08ac7cf1bb65d95bf
Muhammadalif1213 <muhammadalif1213@gmail.com>
2025-01-28 22:53:17 +0700

    Mengganti background top app bar.

--- app/src/main/java/com/example/uas_pam/ui/CustomWidget/TopAppBar.kt
+++ app/src/main/java/com/example/uas_pam/ui/CustomWidget/TopAppBar.kt
@@ -1,6 +1,7 @@
 package com.example.uas_pam.ui.CustomWidget
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Refresh
@@ -9,9 +10,11 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -30,15 +33,26 @@ fun CustomTopAppBar(
                 onRefresh()
             })
         },
-        modifier = modifier,
-        scrollBehavior = scrollBehavior, navigationIcon = {
-            if (canNavigateBack){
+        modifier = modifier.fillMaxWidth(),
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
                 IconButton(
                     onClick = navigateUp
                 ) {
-                    Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = null,
+                        tint = Color.White // Ikon navigasi juga putih
+                    )
                 }
             }
-        }
+        },
+        colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+            containerColor =Color(0xFF2196F3), // Warna background biru
+            navigationIconContentColor = Color.White, // Warna ikon navigasi
+            titleContentColor = Color.White, // Warna teks judul
+            actionIconContentColor = Color.White // Warna ikon aksi
+        )
     )
 }
